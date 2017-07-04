448af01bbf7d1b0dad9f8db9f03ee6fdcc71f1b1
VikRR <rubetc.v.i@gmail.com>
2017-07-04 09:43:35 +0300

    change app service provider register observers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,14 @@
 
 namespace App\Providers;
 
+use App\Model\City;
+use App\Model\Country;
+use App\Model\Language;
+use App\Observers\CityObserver;
+use App\Observers\CountryObserver;
+use App\Observers\LanguageObserver;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +20,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191);
+
+        Country::observe(CountryObserver::class);
+        City::observe(CityObserver::class);
+        Language::observe(LanguageObserver::class);
     }
 
     /**
