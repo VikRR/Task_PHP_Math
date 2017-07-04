2ff7fe8835730d10dcfca36cf51c7eb6ed74b75d
VikRR <rubetc.v.i@gmail.com>
2017-07-04 09:35:38 +0300

    change routes/ web

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,23 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Auth::routes();
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'MainController@index');
+Route::get('edit', 'MainController@show')->name('show.all');
+
+Route::post('add-country', 'CountryController@ajaxStepTwo')->name('country.add');
+Route::resource('country', 'CountryController', ['only' => ['store','edit', 'update', 'destroy']]);
+
+Route::resource('city', 'CityController', ['only' => ['edit', 'update', 'destroy']]);
+Route::post('city/', 'CityController@store')->name('city.store');
+Route::get('find/city', 'CityController@selectCities');
+
+Route::resource('language', 'LanguageController', ['only' => ['edit', 'update', 'destroy']]);
+Route::get('language', 'MainController@language')->name('language.show');
+Route::get('find/language', 'LanguageController@selectLanguages');
+
+Route::post('language/{city}', 'LanguageController@store')->name('language.store');
+
+Route::post('add-city/{country}', 'CityController@ajaxStepThree')->name('city.add');
+
