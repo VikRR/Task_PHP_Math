c93166d1dddbb3b6a3a6718be4bccba98add7773
VikRR <rubetc.v.i@gmail.com>
2017-07-06 11:50:23 +0300

    change main controller clear code

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,9 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Model\City as CityModel;
 use App\Model\Country as CountryModel;
-use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
@@ -16,7 +14,6 @@ class MainController extends Controller
     public function show()
     {
         $countries = CountryModel::with('city.lang')->get();
-        //dd($countries);
 
         return view('show_all', compact('countries'));
     }
@@ -24,18 +21,7 @@ class MainController extends Controller
     public function language()
     {
         $countries = CountryModel::pluck('country', 'id');
-        //dd($countries);
 
         return view('languages', compact('countries'));
     }
-
-    //public function selectCities(Request $request)
-    //{
-    //    $data = CityModel::select('id', 'city')
-    //        ->where('country_id', $request->id)
-    //        ->get();
-    //
-    //
-    //    return response()->json($data);
-    //}
 }
