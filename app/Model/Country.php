3c2bb7c3493c3ca1bfdf7a8ca2cbf9764a7bcadd
VikRR <rubetc.v.i@gmail.com>
2017-07-06 11:51:07 +0300

    change country model clear code

--- app/Model/Country.php
+++ app/Model/Country.php
@@ -3,11 +3,9 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Country extends Model
 {
-    //use SoftDeletes;
 
     protected $fillable = [
         'country',
