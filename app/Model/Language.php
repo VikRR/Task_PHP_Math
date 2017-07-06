1d47151c2733fb986cb8a2194d1b422ce740c2f5
VikRR <rubetc.v.i@gmail.com>
2017-07-06 11:51:20 +0300

    change language model clear code

--- app/Model/Language.php
+++ app/Model/Language.php
@@ -3,11 +3,9 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Language extends Model
 {
-    //use SoftDeletes;
 
     protected $table = 'languages';
 
