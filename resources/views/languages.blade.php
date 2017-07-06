80d35b980756cbd595e13025c031396dab484f14
VikRR <rubetc.v.i@gmail.com>
2017-07-06 11:52:43 +0300

    change  view language clear code

--- resources/views/languages.blade.php
+++ resources/views/languages.blade.php
@@ -6,7 +6,6 @@
         <div class="panel panel-default">
             <div class="panel-body">
                 <div class="col-md-offset-2 col-md-8">
-                    {{--<legend>Добавьте название страны</legend>--}}
                     {!! Form::open(['route'=>'country.add', 'id'=>'form-add-country']) !!}
                     <div class="col-md-6">
                         <div class="form-group">
@@ -17,16 +16,14 @@
                     <div class="col-md-6">
                         <div class="form-group">
                             {{Form::label('city', 'Выберите город:')}}
-                            {{--{{Form::select('city', null, ['placeholder' => 'Выберите город...', 'id'=>'city', 'class'=>'form-control']) }}--}}
                             <select id="city" class="form-control">
                                 <option selected  value="0">Выберите город...</option>
                             </select>
                         </div>
                     </div>
-                    {{--{{Form::submit('Next',['class'=>'btn btn-success'])}}--}}
                     {!! Form::close() !!}
                     <h3 id="title"></h3>
-                    <ul id="lang"></ul>
+                    <div id="lang"></div>
                 </div>
             </div>
         </div>
