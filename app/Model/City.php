dd3988f672bc4037c7fab756ee1f81b848ff25e0
VikRR <rubetc.v.i@gmail.com>
2017-07-06 11:50:51 +0300

    change city model clear code

--- app/Model/City.php
+++ app/Model/City.php
@@ -3,12 +3,9 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class City extends Model
 {
-    //use SoftDeletes;
-
     protected $table = 'cities';
 
     protected $fillable = [
